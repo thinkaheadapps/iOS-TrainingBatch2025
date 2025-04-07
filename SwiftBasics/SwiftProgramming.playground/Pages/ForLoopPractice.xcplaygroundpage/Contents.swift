51d67304df38c5bfdd77274591abcfa300f3954e
Divya Rai <thinkaheadapps@gmail.com>
2025-04-07 20:28:51 +0530

    For loop basic level

---
+++ SwiftBasics/SwiftProgramming.playground/Pages/ForLoopPractice.xcplaygroundpage/Contents.swift
@@ -0,0 +1,72 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/**
+ Basic Level
+ 
+
+ Print even numbers from 2 to 20.
+
+ Print the multiplication table of 5.
+
+ Print the square of numbers from 1 to 10.
+
+ Print elements of an array:
+
+ */
+
+//Print numbers from 1 to 10.
+//Including and excluding 10
+
+for i in 1...10 {
+    print(i)
+}
+
+//exclusive 10
+for i in 1..<10 {
+    print(i)
+}
+
+//print even number from 2- 20
+for number in stride(from: 2, to: 20, by: 2) {
+    print(number)
+}
+
+//Print the multiplication table of 5.
+for number in 1...10 {
+    print(number*5)
+}
+
+// Print the square of numbers from 1 to 10.
+for number in 1...10 {
+    print(number*number)
+}
+
+//print element of array
+let fruits = ["apple", "Banana", "Cherry", "Mango"]
+for fruit in fruits {
+print(fruit)
+}
+
+/**
+ 🔁 Intermediate Level
+ Sum of first 100 natural numbers.
+
+ Find the factorial of a given number.
+
+ Reverse the characters of a string using a loop.
+
+ Print all prime numbers between 1 and 100.
+
+ Count vowels in a string.
+ */
+
+    /**
+     Advance level
+     *
+     **
+     ***
+     ****
+
+     */
