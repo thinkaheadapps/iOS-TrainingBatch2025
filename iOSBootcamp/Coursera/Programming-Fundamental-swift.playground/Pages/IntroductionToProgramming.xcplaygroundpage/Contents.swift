99b08fa2504e858f2da7fa71721f9b39d0088683
Divya Rai <thinkaheadapps@gmail.com>
2025-05-08 23:05:46 +0530

    Swift Module 1 Working with constants and variables

---
+++ iOSBootcamp/Coursera/Programming-Fundamental-swift.playground/Pages/IntroductionToProgramming.xcplaygroundpage/Contents.swift
@@ -0,0 +1,40 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var count = 0
+while count<10 {
+    count += 1
+    print(count)
+}
+
+//Constant
+let name = "Divya"
+print(name)
+
+//variable
+var age = 20
+print(age)
+
+age = 21
+print(age)
+
+let day = "Monday"
+let dailyTemprature = 75
+
+print("Today is \(day)")
+
+print("The temprature on \(day) is \(dailyTemprature)")
+
+var temperature = 70
+print("The temprature on \(day) is \(temperature) °F.")
+
+temperature = 80
+print("The temperature on \(day) evening is \(temperature) °F.")
+
+var weeklyTemperature = 75
+temperature = weeklyTemperature
+print("weeklyTemperature value is \(weeklyTemperature)")
+
+
+
