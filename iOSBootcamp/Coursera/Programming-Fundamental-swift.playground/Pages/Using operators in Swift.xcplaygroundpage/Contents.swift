ea82e302f4f5655f028d86e0c46f804802944598
Divya Rai <thinkaheadapps@gmail.com>
2025-05-09 09:29:07 +0530

    Module 1: Using operators in swift

---
+++ iOSBootcamp/Coursera/Programming-Fundamental-swift.playground/Pages/Using operators in Swift.xcplaygroundpage/Contents.swift
@@ -0,0 +1,44 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var firstValue = 10
+firstValue = firstValue * 14
+print("firstValue is \(firstValue)")
+
+//: [Next](@next)
+//Compound assignment operator
+
+ var basket = 10 % 2
+print("basket = \(basket)")
+
+basket = 11 % 2
+print("basket odd number = \(basket)")
+
+    /*
+     Brackets
+     Indices
+     Division
+     Multiplication
+     Addition
+     Subtraction
+     */
+
+// Unary minus operator
+
+let one = 1
+let negativeValue = -one
+print("Urinary minus of one = \(negativeValue)")
+
+// Numeric type conversion in swift
+
+//
+
+let x = 5
+let y = 0.33
+let z = Double(x) + y
+print("Value of z = \(z)")
+
+
+
+
