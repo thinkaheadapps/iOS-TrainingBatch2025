b09bac2bbf0728d9a4b0099fdfcf4a42a8b11e77
Divya Rai <thinkaheadapps@gmail.com>
2025-05-09 17:11:31 +0530

    Exercise: Work with conditional statements in Swift

---
+++ iOSBootcamp/Coursera/Programming-Fundamental-swift.playground/Pages/Switch statements.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var greeting = "Switch statements"
+
+let language = "English"
+switch language {
+    case "English": print("The default language is set to English.")
+    case "Spanish": print("The default language is set to Spanish.")
+    default: print("The default language has not been configured yet.")
+}
